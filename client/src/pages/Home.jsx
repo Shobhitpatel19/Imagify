f58da292ce4ebe5f4007b14818674fd951f89fa6
githubanujmishra <anujmishra2702@gmail.com>
2024-07-17 22:51:23 +0530

    ui updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -114,36 +114,65 @@ const Home = () => {
     <>
       <Navbar btnText="Community" />
 
-      {/* Dropdown Menu */}
-      <div>Select a Model</div>
-      <PopupState variant="popover" popupId="demo-popup-menu">
-        {(popupState) => (
-          <>
-            <Button variant="contained" {...bindTrigger(popupState)}>
-              {selectedModel}
-            </Button>
-            <Menu {...bindMenu(popupState)}>
-              {models.map((model, index) => (
-                <MenuItem
-                  key={index}
-                  onClick={() => {
-                    setSelectedModel(model);
-                    popupState.close();
-                  }}
-                >
-                  {model}
-                </MenuItem>
-              ))}
-            </Menu>
-          </>
-        )}
-      </PopupState>
+      <div className="bg-black ">
+      <div className="text-white text-center font-bold text-6xl font-['Mona-Sans Light'] pt-2">Create <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-violet-500 to-blue-500"> Powerful</span> model or <br />image in seconds</div>
+      <div className="text-white px-2 pt-8 text-center">Meet Imagify. the powerful photo generator who will turn any image into a unique artwork in seconds.</div>
+        {/* Dropdown Menu */}
+      {/* <div className="text-3xl">
+          <PopupState  variant="popover" popupId="demo-popup-menu">
+            {(popupState) => (
+              <>
+                <Button  variant="contained" {...bindTrigger(popupState)}>
+                  {selectedModel}
+                </Button>
+                <Menu {...bindMenu(popupState)}>
+                  {models.map((model, index) => (
+                    <MenuItem
+                      key={index}
+                      onClick={() => {
+                        setSelectedModel(model);
+                        popupState.close();
+                      }}
+                    >
+                      {model}
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )}
+          </PopupState>
+      </div> */}
 
+      <div className="flex items-center justify-center pt-8 z-50">
+      <div class="main-wrapper relative group">
+        <button class="rounded-lg text-white border-2 border-gray-500 w-52 py-3 px-2 flex justify-between shadow-md">
+                Enter generator name 
+        </button>
+        <ul class="z-50 dropdown-menu group-hover:block hidden absolute text-gray-700 pt-1  ">
+          <li>
+            <a href="#" class="w-[210px] mb-1 rounded-md  bg-gray-200 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap" >stableDiffusion</a>
+          </li>
+          <li>
+            <a href="#" class="w-[210px] mb-1 rounded-md  bg-gray-200 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap" >AbsoluteReality</a>
+          </li>
+          <li>
+            <a href="#" class="w-[210px] mb-1 rounded-md  bg-gray-200 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap" >Playground</a>
+          </li>
+          <li>
+            <a href="#" class="w-[210px] mb-1 rounded-md  bg-gray-200 hover:bg-gray-300 py-2 px-4 block whitespace-no-wrap" >Runwayml</a>
+          </li>
+          
+</ul>
+  </div>
+      </div>
+
+
+<div className="flex w-full justify-center flex-col items-center">
       {/* Name Input Field */}
-      <div className="mt-4">
+      <div className="w-full flex flex-col items-center  ">
         <label
           htmlFor="name"
-          className="block text-sm font-medium text-gray-700"
+          // className="block text-sm font-medium text-white"
         >
           Name
         </label>
@@ -153,33 +182,48 @@ const Home = () => {
           name="name"
           value={name}
           onChange={handleNameChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className=" block w-[30%] px-3 py-2 border bg-transparent border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white mx-8"
           placeholder="Enter your name"
         />
       </div>
 
       {/* Prompt Input Field */}
-      <div className="mt-4">
-        <label
+      <div className="w-full flex flex-col items-center px-4 mt-3">
+        {/* <label
           htmlFor="prompt"
-          className="block text-sm font-medium text-gray-700"
+          // className="block text-sm font-medium text-gray-700"
         >
           Prompt
-        </label>
-        <input
+        </label> */}
+ <div className="flex w-full items-center justify-center gap-1">
+ <input
           type="text"
           id="prompt"
           name="prompt"
           value={prompt}
           onChange={handlePromptChange}
-          className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          className="mt-1 block w-[40%] px-3 py-2 border border-gray-600 bg-transparent rounded-xl shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           placeholder="Enter your prompt"
         />
+
+<button
+          onClick={handleGenerate}
+          className="px-2 py-[5.5px] text-blue-500 hover:bg-blue-900 uppercase border-blue-600 border-2 rounded-lg shadow-md  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        >
+          {
+            loading ? "Generating..." : "Generate"
+
+          }
+        </button>
+  </div>       
+       
       </div>
 
+</div>      
+
       {/* Image Box */}
-      <div className="mt-4 flex justify-center">
-        <div className="relative w-64 h-64 bg-gray-200 rounded-md overflow-hidden">
+      <div className="mt-4 flex justify-center items-center">
+        <div className="flex justify-center items-center relative w-72 h-72 bg-gray-900 rounded-md overflow-hidden">
           {loading ? (
             <Loader />
           ) : (
@@ -191,21 +235,12 @@ const Home = () => {
           )}
         </div>
       </div>
-
+<div className="flex items-center justify-center gap-1 pb-10">
       {/* Generate and Download Button */}
-      <div className="mt-4 flex justify-between">
-        <button
-          onClick={handleGenerate}
-          className="px-6 py-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          {
-            loading ? "Generating..." : "Generate"
-
-          }
-        </button>
+      <div className="mt-4 text-center">
         <button
           onClick={handleDownload}
-          className="px-6 py-3 bg-green-500 text-white rounded-md shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+          className="px-2 py-2 text-green-500 border-2 border-green-700 rounded-md shadow-md hover:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
         >
           Download
         </button>
@@ -213,12 +248,16 @@ const Home = () => {
 
       {/* Share Button */}
       <div>
-        <button
+</div>   
+
+<button
           onClick={handleShare}
-          className="px-6 py-3 bg-purple-500 text-white rounded-md shadow-md hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50"
+          className="px-2 py-2 text-purple-500 border-2 border-violet-600 rounded-md shadow-md hover:bg-purple-900   focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-opacity-50 mt-4"
         >
           Share with Community
         </button>
+</div>        
+        
       </div>
     </>
   );
